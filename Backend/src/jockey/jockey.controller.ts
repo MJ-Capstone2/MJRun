71e622338d575b930d989eed9630a497a58ad3fd
dojinyou <djyou128@gmail.com>
2021-11-03 20:41:44 +0900

    install prisma

---
+++ Backend/src/jockey/jockey.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('jockey')
+export class JockeyController {}
---
+++ Backend/src/raceresult/raceresult.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('raceresult')
+export class RaceresultController {}
