49bcd5acb5b2d17966322e82a47d3dc0201c5719
SANGEUI <81904690+pianovel@users.noreply.github.com>
2021-11-11 18:50:48 +0900

    horse entity build
    horse>horse.entity.ts file create & horse entity build

--- Backend/src/horse/horse.entity.ts
+++ Backend/src/horse/horse.entity.ts
@@ -1,18 +1,24 @@
 import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity()
-export class Jockey extends BaseEntity {
+export class Horse extends BaseEntity {
   @PrimaryColumn({ type: 'int', length: 20 })
   number: number;
 
-  @Column({ type: 'varchar2', length: 10 })
+  @Column({ type: 'varchar2', length: 20 })
   name: string;
 
   @Column({ type: 'tinyint', length: 2 })
   age: number;
 
-  @Column({ type: 'int', length: 8 })
-  career: number;
+  @Column({ type: 'tinyint', length: 1 })
+  sex: number;
+
+  @Column({ type: 'char', length: 4 })
+  nationality: string;
+
+  @Column({ type: 'varchar2', length: 10 })
+  grade: string;
 
   @Column({ type: 'int', length: 8 })
   total_score: number;
@@ -21,10 +27,16 @@ export class Jockey extends BaseEntity {
   recent_score: number;
 
   @Column({ type: 'float', length: 4 })
-  total_win_rate: number;
+  total_double_win_rate: number;
 
   @Column({ type: 'float', length: 4 })
-  recent_win_rate: number;
+  recent_double_win_rate: number;
+
+  @Column({ type: 'int', length: 16 })
+  total_prize_money: number;
+
+  @Column({ type: 'int', length: 16 })
+  recent_1year_prize_money: number;
 
   @Column({ type: 'tinyint', length: 2})
   recent_1year_ranking: number;
