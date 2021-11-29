48ed68d8daacac5b245bf4448113a37218b123e4
Hyejin00 <44718809+Hyejin00@users.noreply.github.com>
2021-11-30 00:57:34 +0900

    layout변경 및 관리자페이지 read

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -18,9 +18,8 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/guide" component={Guide} />
           <Route path="/prediction" component={Prediction} />
-          <Route exact path="/admin" component={Admin} />
-          <Route exact path="/admin/upload" component={Admin} />
           <Route exact path="/admin/login" component={Login} />
+          <Route exact path="/admin/:dtype" component={Admin} />
           <Route path="*" component={NotFound} />
         </Switch>
         {/* <Footer /> */}
--- Frontend/src/layout/AdminLayout.js
+++ Frontend/src/layout/AdminLayout.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from '../components/admin/Navbar'
+import SideNav from '../components/admin/SideNav';
+import CardBox from '../components/admin/CardBox';
 import {
   makeStyles,
   CircularProgress
@@ -13,6 +15,24 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
     paddingBottom: '5em'
+  },
+  root_container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  nav_container: {
+    display: 'flex',
+    justifyContent: 'center',
+    height: 'fit-content',
+    position: 'fixed',
+    zIndex: 1,
+    width: '22em'
+  },
+  content_container: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginLeft: '22em'
   }
 }));
 
@@ -23,14 +43,23 @@ const AdminLayout = ({children, loading=false}) => {
   return (
     <div>
       <Navbar />
-      <main>
-        {loading?
-        <div className={classes.spiner}>
-          <CircularProgress />
-        </div>:
-        children
-        }
-      </main>
+      <div className={classes.root_container}>
+        <div>
+          <div className={classes.nav_container}>
+            <SideNav />
+          </div>
+          <div className={classes.content_container}>
+            {loading?
+            <div className={classes.spiner}>
+              <CircularProgress />
+            </div>:
+            <CardBox>
+              {children}
+            </CardBox>
+            }
+          </div>
+        </div>
+      </div>
     </div>
 
   )
