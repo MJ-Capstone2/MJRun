1d190438ca4304f2b85570df1d3847252b3402ba
Hyejin00 <44718809+Hyejin00@users.noreply.github.com>
2021-12-02 17:26:05 +0900

    csv 업로드뷰 수정

--- Frontend/src/components/admin/UploadModal.js
+++ Frontend/src/components/admin/UploadModal.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   makeStyles,
   Modal,
   Fade,
   Backdrop,
-  Button
+  Button,
+  Typography,
+  IconButton
 } from '@material-ui/core';
 import NoteAddIcon from '@material-ui/icons/NoteAdd';
+import ClearIcon from '@material-ui/icons/Clear';
 
-const useToolbarStyles = makeStyles((theme) => ({
+const useModalStyles = makeStyles((theme) => ({
   input: {
     display: 'none',
   },
@@ -37,11 +40,22 @@ const useToolbarStyles = makeStyles((theme) => ({
     display: 'flex',
     alignItems:'center',
     justifyContent: 'center'
+  },
+  list_container: {
+    display: 'flex'
   }
 }));
 
 const UploadModal = ({open, handleClose}) => {
-  const classes = useToolbarStyles();
+  const classes = useModalStyles();
+  const [ files, setFiles ] = useState([]);
+  const handleChange = (e) => {
+    var fileArr = Array.prototype.slice.call(e.target.files);
+    setFiles([...files, ...fileArr]);
+  }
+  const removeFile = (f) => {
+    setFiles(files.filter(x => x !== f));
+  }
   return (
     <Modal
       aria-labelledby="transition-modal-title"
@@ -59,14 +73,34 @@ const UploadModal = ({open, handleClose}) => {
         <div className={classes.paper}>
           <h2 id="transition-modal-title">데이터 추가</h2>
           <div className={classes.input_container}>
-            <NoteAddIcon color="disabled" style={{ fontSize: 100 }}/>
-            <p id="transition-modal-description">csv 파일만 선택 가능합니다.</p>
+            {
+              files.length === 0?(
+                <>
+                  <NoteAddIcon color="disabled" style={{ fontSize: 100 }}/>
+                  <p id="transition-modal-description">csv 파일만 선택 가능합니다.</p>
+                </>
+              ):(
+                files.map((file,idx)=>{
+                  let keyName = `${file.name}-${idx}`;
+                  return (
+                  <div key={keyName} className={classes.list_container}>
+                    <Typography>{file.name}</Typography>
+                    <IconButton aria-label="delete" onClick={()=>(removeFile(file))} size="small">
+                      <ClearIcon />
+                    </IconButton>
+                  </div>
+                  );
+                })
+              )
+            }
+            
             <input
               accept=".csv"
               className={classes.input}
               id="contained-button-file"
               multiple
               type="file"
+              onChange={handleChange}
             />
             <label htmlFor="contained-button-file">
               <Button variant="contained" color="primary" component="span">
